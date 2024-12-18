8efdec997c59022979bac9eb82a360e144a85b12
Maxime Biechy <maxime.biechy5@etu.univ-lorraine.fr>
2024-12-18 10:12:34 +0100

    instance de la bd

--- backend/src/config/dependencies.ts
+++ backend/src/config/dependencies.ts
@@ -1,4 +1,7 @@
 import PodcastService from "../core/use_cases/podcast/PodcastService";
 import PodcastRepository from "../infrastructure/mongodb/PodcastRepository";
+import {getMongoClient} from "../infrastructure/mongodb/MongoClient";
 
-export const podcastServiceInterface = new PodcastService(new PodcastRepository());
+const mongoClient = getMongoClient();
+
+export const podcastServiceInterface = new PodcastService(new PodcastRepository(mongoClient));
