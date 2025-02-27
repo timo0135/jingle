427f9a063aa2941e952ebb413da992be10e7854a
MaximeBiechy <maxime.biechy5@etu.univ-lorraine.fr>
2025-02-27 15:30:06 +0100

    feat: integrate AudioPlayer and ShowsContainer components for enhanced podcast experience

--- frontend/components/ShowsContainer.vue
+++ frontend/components/ShowsContainer.vue
@@ -1,128 +1,31 @@
 <script setup lang="ts">
 import ShowCard from "~/components/cards/showCard.vue";
-import {onMounted, ref} from "vue";
-import {useAPI} from "#imports";
-import {useUserStore} from "~/stores/userStore";
-
-const api = useAPI();
-const userStore = useUserStore();
-
-const emit = defineEmits(['changeVisibility']);
 
 const props = defineProps<{
   title: string;
+  podcasts: Array<{
+    id: string;
+    name: string;
+    date: string;
+    description: string;
+    isFavorite: boolean;
+    audioUrl: string;
+    image: string;
+  }>;
 }>();
 
-interface Podcast {
-  id: string;
-  name: string;
-  date: string;
-  description: string;
-  isFavorite: boolean;
-  audioUrl: string;
-  image: string;
-}
-
-let podcasts = ref<Podcast[]>([]);
-let favoritePlaylistId = ref<string | null>(null);
-
-async function getPodcasts() {
-  try {
-    const response = await api.get('/podcasts');
-    if (!response.data.podcasts) return;
-
-    podcasts.value = await Promise.all(response.data.podcasts.map(async (podcast: any) => {
-      const imageResponse = await api.get(`/podcasts/${podcast.id}`);
-      return {
-        ...podcast,
-        isFavorite: false,
-        audioUrl: podcast.file,
-        image: imageResponse.data.podcast.image,
-      };
-    }));
-
-    if (userStore.user_id) {
-      await getFavoritePlaylist();
-      await getFavoritePodcasts();
-    }
-  } catch (error: any) {
-    const message = error.response?.data?.message || 'Une erreur est survenue';
-    userStore.showErrorToast(message);
-  }podcasts
-}
-
-async function getFavoritePlaylist() {
-  try {
-    const response = await api.get(`users/${userStore.user_id}/playlists`, {
-      headers: {
-        Authorization: `Bearer ${userStore.user_token}`,
-      },
-    });
-    if (!response.data.playlists) return;
-    const favoritePlaylist = response.data.playlists.find((playlist: any) => playlist.name === 'favoris');
-    if (favoritePlaylist) {
-      favoritePlaylistId.value = favoritePlaylist.id;
-    }
-  } catch (error: any) {
-    userStore.showErrorToast(error.response.data.message);
-  }
-}
-
-async function getFavoritePodcasts() {
-  if (!favoritePlaylistId.value) return;
-
-  try {
-    const response = await api.get(`playlists/${favoritePlaylistId.value}`, {
-      headers: {
-        Authorization: `Bearer ${userStore.user_token}`,
-      },
-    });
-    if (!response.data.podcast) return;
-    const favoritePodcasts = response.data.podcast.content;
-    podcasts.value.forEach(podcast => {
-      if (favoritePodcasts.some((fav: any) => fav.id === podcast.id)) {
-        podcast.isFavorite = true;
-      }
-    });
-  } catch (error: any) {
-    userStore.showErrorToast(error.response.data.message);
-  }
-}
+const emit = defineEmits(['changeVisibility']);
 
 function handleShowCardClicked(audioUrl: string, name: string, description: string, image: string) {
   emit('changeVisibility', false, true, audioUrl, name, description, image);
 }
-
-onMounted(async () => {
-  const showsContainer: HTMLElement | null = document.getElementById('shows_container');
-  const flecheGauche: HTMLElement | null = document.getElementById('flecheGauche');
-  const flecheDroit: HTMLElement | null = document.getElementById('flecheDroit');
-
-  if (flecheGauche && showsContainer) {
-    flecheGauche.addEventListener('click', () => {
-      if (showsContainer) {
-        showsContainer.scrollLeft -= 150;
-      }
-    });
-  }
-
-  if (flecheDroit && showsContainer) {
-    flecheDroit.addEventListener('click', () => {
-      if (showsContainer) {
-        showsContainer.scrollLeft += 150;
-      }
-    });
-  }
-
-  await getPodcasts();
-});
 </script>
 
 <template>
   <div class="font-bold font-inter mx-8 my-8 text-primary">
     <h2 class="my-4 text-3xl underline">{{ props.title }}</h2>
     <div class="flex gap-4 overflow-x-scroll no-scrollbar overflow-auto" id="shows_container">
-      <ShowCard v-for="podcast in podcasts" :key="podcast.id" :id="podcast.id" :name="podcast.name"
+      <ShowCard v-for="podcast in props.podcasts" :key="podcast.id" :id="podcast.id" :name="podcast.name"
                 :date="podcast.date" :description="podcast.description" :isFavorite="podcast.isFavorite"
                 :audioUrl="podcast.audioUrl"
                 @showCardClicked="handleShowCardClicked(podcast.audioUrl, podcast.name, podcast.description, podcast.image)"/>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,15 +1,21 @@
 <script setup lang="ts">
-
 import HeroSection from "~/components/HeroSection.vue";
 import ShowsContainer from "~/components/ShowsContainer.vue";
 import AudioPlayer from "~/components/AudioPlayer.vue";
+import {ref, onMounted} from 'vue';
+import {useAPI} from "#imports";
+import {useUserStore} from "~/stores/userStore";
 
+const api = useAPI();
+const userStore = useUserStore();
 const directVisible = ref(false);
 const podcastVisible = ref(false);
 const selectedAudioUrl = ref<string | null>(null);
 const selectedName = ref('');
 const selectedDescription = ref('');
 const selectedImage = ref('');
+const podcasts = ref([]);
+const favoritePodcasts = ref<string[]>([]);
 
 const changeVisibility = (direct: boolean, podcast: boolean, audioUrl: string | null = null, name: string, description: string, image: string) => {
   directVisible.value = direct;
@@ -20,25 +26,64 @@ const changeVisibility = (direct: boolean, podcast: boolean, audioUrl: string | 
   selectedImage.value = image;
 };
 
+async function getFavoritePodcasts() {
+  try {
+    const response = await api.get(`users/${userStore.user_id}/playlists`, {
+      headers: {
+        Authorization: `Bearer ${userStore.user_token}`,
+      },
+    });
+    const favoritePlaylist = response.data.playlists.find((playlist: any) => playlist.name === 'favoris');
+    if (favoritePlaylist) {
+      const favoriteResponse = await api.get(`playlists/${favoritePlaylist.id}`, {
+        headers: {
+          Authorization: `Bearer ${userStore.user_token}`,
+        },
+      });
+      favoritePodcasts.value = favoriteResponse.data.podcast.content.map((podcast: any) => podcast.id);
+    }
+  } catch (error: any) {
+    console.error('Error fetching favorite podcasts:', error);
+  }
+}
+
+async function getPodcasts() {
+  try {
+    const response = await api.get('/podcasts');
+    if (!response.data.podcasts) return;
+
+    podcasts.value = await Promise.all(response.data.podcasts.map(async (podcast: any) => {
+      const imageResponse = await api.get(`/podcasts/${podcast.id}`);
+      return {
+        ...podcast,
+        isFavorite: favoritePodcasts.value.includes(podcast.id),
+        audioUrl: podcast.file,
+        image: imageResponse.data.podcast.image,
+      };
+    }));
+  } catch (error: any) {
+    console.error('Error fetching podcasts:', error);
+  }
+}
+
+onMounted(async () => {
+  await getFavoritePodcasts();
+  await getPodcasts();
+});
 </script>
 
 <template>
   <div class="relative overflow-hidden" id="landing">
     <NavbarComponent/>
     <HeroSection @changeVisibility="changeVisibility"/>
-
-
-    <!-- Shows container -->
-    <ShowsContainer :title="'Podcasts au top'" @changeVisibility="changeVisibility"/>
-
+    <ShowsContainer :title="'Podcasts au top'" :podcasts="podcasts" @changeVisibility="changeVisibility"/>
     <AudioPlayer v-if="directVisible && !podcastVisible" :direct="true"/>
     <AudioPlayer v-if="podcastVisible && !directVisible" :direct="false" :audioUrl="selectedAudioUrl"
                  :name="selectedName" :description="selectedDescription" :image="selectedImage"/>
     <Footer/>
   </div>
-
-
 </template>
 
 <style scoped>
+/* Add your styles here */
 </style>
